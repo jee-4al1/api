a9db82a5913efcff5bfba6f4aba1bea2914cb943
Ifzas Nguyen <nguyen.ifzas@gmail.com>
2022-06-03 01:13:25 +0200

    initial commit

---
+++ src/test/java/com/esgi/jee/project/jeeproject/JeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.jee.project.jeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
